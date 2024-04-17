4b443a45b53ba6a52c18525ca426194ce4c6ec4d
Dyllan Tinoco <dyllan@tinoco.casa>
2024-04-17 18:00:30 -0500

    Switch to using flakes

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake for my desktop";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nvidia-patch.url = "github:icewind1991/nvidia-patch-nixos";  
+    nvidia-patch.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.Dyllans-Desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jdoe = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+      ];
+    };
+  };
+}
