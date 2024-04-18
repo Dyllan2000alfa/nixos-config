c0c3d24f10ec01122bba365c4ca9af2e5a4ab70f
Dyllan Tinoco <dyllan@tinoco.casa>
2024-04-17 22:52:57 -0500

    Finally get nvidia-patch working

--- flake.nix
+++ flake.nix
@@ -23,8 +23,6 @@
       Dyllans-Desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
-
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
@@ -36,6 +34,48 @@
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
+          ({ config, pkgs, ... }: 
+            let
+              # nvidia package to patch
+              package = config.boot.kernelPackages.nvidiaPackages.stable;
+            in
+            { 
+              nixpkgs.overlays = [inputs.nvidia-patch.overlays.default];
+              # Enable nvidia driver
+              hardware.nvidia = {
+
+                # Modesetting is required.
+                modesetting.enable = true;
+
+                # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+                # Enable this if you have graphical corruption issues or application crashes after waking
+                # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+                # of just the bare essentials.
+                powerManagement.enable = false;
+
+                # Fine-grained power management. Turns off GPU when not in use.
+                # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+                powerManagement.finegrained = false;
+
+                # Use the NVidia open source kernel module (not to be confused with the
+                # independent third-party "nouveau" open source driver).
+                # Support is limited to the Turing and later architectures. Full list of 
+                # supported GPUs is at: 
+                # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+                # Only available from driver 515.43.04+
+                # Currently alpha-quality/buggy, so false is currently the recommended setting.
+                open = false;
+
+                # Enable the Nvidia settings menu,
+	            # accessible via `nvidia-settings`.
+                nvidiaSettings = true;
+
+                # Optionally, you may need to select the appropriate driver version for your specific GPU.
+                package = pkgs.nvidia-patch.patch-nvenc (pkgs.nvidia-patch.patch-fbc package);
+              };
+            })
+
+          ./configuration.nix          
         ];
       };
     };
