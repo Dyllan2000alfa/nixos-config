1c2669560424053785bcd568be43d68885d06b4d
Dyllan Tinoco <dyllan@tinoco.casa>
2024-04-23 16:46:19 -0500

    Rename nix-flatpak to flatpak

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
+    flatpaks.url = "github:gmodena/nix-flatpak/?ref=v0.4.1";
     nvidia-patch = {
         url = "github:icewind1991/nvidia-patch-nixos";  
         inputs.nixpkgs.follows = "nixpkgs";
