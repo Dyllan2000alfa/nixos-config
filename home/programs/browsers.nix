e83648ec504b2ced37f2c8b25c8f54f7ab9f9a2b
Dyllan Tinoco <dyllan@tinoco.casa>
2024-04-23 16:39:31 -0500

    Add flatpak librewolf

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -1,10 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
+  imports = [ inputs.flatpaks.homeManagerModules.nix-flatpak ];
+
   programs = {
     #Enable firefox
     firefox = {
       enable = true;
     };
   };
+
+  # Install flatpak librewolf
+  services.flatpak.packages = [
+    { appId = "io.gitlab.librewolf-community"; origin = "flathub";  }
+  ];
 }
