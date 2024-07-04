9a708f6ab78c7b6fd81e228f947ff9a0c28b2118
Dyllan Tinoco <dyllan@tinoco.casa>
2024-07-04 12:18:25 -0500

    Add lfe mix to pipewire

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -17,40 +17,27 @@
     rnnoise-plugin
   ];
 
-  services.pipewire.extraConfig.pipewire."99-input-denoising" = {
-    context.modules = [
-      {
-        name = libpipewire-module-filter-chain;
-        args = {
-          node.description =  "Noise Canceling source";
-          media.name =  "Noise Canceling source";
-          filter.graph = {
-            nodes = [
-              {
-                type = ladspa;
-                name = rnnoise;
-                plugin = /run/current-system/sw/lib/ladspa/librnnoise_ladspa.so
-                label = noise_suppressor_mono;
-                control = {
-                  "VAD Threshold (%)" = 50.0;
-                  "VAD Grace Period (ms)" = 200;
-                  "Retroactive VAD Grace (ms)" = 0;
-                };
-              }
-            ]
-          };
-          capture.props = {
-            node.name =  "capture.rnnoise_source";
-            node.passive = true;
-            audio.rate = 48000;
-          };
-          playback.props = {
-            node.name =  "rnnoise_source";
-            media.class = Audio/Source;
-            audio.rate = 48000;
-          };
-        };
-      }
-    ]
+  services.pipewire.extraConfig.pipewire-pulse."40-mix-lfe" = {
+    "stream.properties" = {
+      "channelmix.upmix"      = true;
+      "channelmix.upmix-method" = "psd";
+      "channelmix.lfe-cutoff" = 150;
+    };
+  };
+
+  services.pipewire.extraConfig.client."40-mix-lfe" = {
+    "stream.properties" = {
+      "channelmix.upmix"      = true;
+      "channelmix.upmix-method" = "psd";
+      "channelmix.lfe-cutoff" = 150;
+    };
+  };
+
+  services.pipewire.extraConfig.client-rt."40-mix-lfe" = {
+    "stream.properties" = {
+      "channelmix.upmix"      = true;
+      "channelmix.upmix-method" = "psd";
+      "channelmix.lfe-cutoff" = 150;
+    };
   };
 }
