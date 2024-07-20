1f0c3f6e77a07e1a054b0580ed664f7c0d3058c5
Dyllan Tinoco <dyllan@tinoco.casa>
2024-07-20 13:49:04 -0500

    Add gaming module

---
+++ modules/gaming.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+{
+  #Allow steam to install unfree packages
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-original"
+    "steam-run"
+  ];
+
+  # Install steam
+  programs.steam = {
+    enable=true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+}
+
