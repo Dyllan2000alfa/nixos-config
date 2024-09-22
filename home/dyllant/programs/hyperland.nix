00ddf5ebf30aa6e32f73cdeed207e3874f51f985
Dyllan Tinoco <dyllan@tinoco.casa>
2024-09-21 20:12:20 -0500

    Add some config for hyprland

--- home/dyllant/programs/hyperland.nix
+++ home/dyllant/programs/hyperland.nix
@@ -1,5 +1,9 @@
 { config, pkgs, inputs, ... }:
 {
+
+    wayland.windowManager.hyprland.package = true;
+
+  # Packages need for hypland
   home.packages = with pkgs; [
     waybar
     hyprpaper
@@ -8,5 +12,14 @@
     xdg-desktop-portal-gtk
     qt5-wayland
     qt6-wayland
+    mako
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-1, 1440x900@75, 0x0, 1"
+      "DP-2, 1920x1080@75, 1440x0,1"
+      "DP-3, 1600x900@60, 1680x-1080, 1"
+    ];
+  };
 }
--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,16 +1,23 @@
 { pkgs, ... }:
 {
-  programs.hyprland = {
-    # Install the packages from nixpkgs
-    enable = true;
-    # Whether to enable XWayland
-    xwayland.enable = true;
-  };
+  # Enable Hyprland
+  programs.hyprland.enable = true;
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
   environment.systemPackages = with pkgs; [
     kitty # required for the default Hyprland config
+    greetd.tuigreet
   ];
 
-  # Optional, hint electron apps to use wayland:
-  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # Enable Display Manager
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 }
