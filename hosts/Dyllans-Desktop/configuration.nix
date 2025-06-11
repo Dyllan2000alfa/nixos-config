640d2a3c740d117a05dd5cae386bb570535e1579
Dyllan Tinoco <dyllan@tinoco.casa>
2025-06-10 19:11:17 -0500

    Update user groups

--- hosts/Dyllans-Desktop/configuration.nix
+++ hosts/Dyllans-Desktop/configuration.nix
@@ -84,7 +84,20 @@
     isNormalUser = true;
     uid = 1000;
     description = "Dyllan Tinoco";
-    extraGroups = ["wheel" "networkmanager" "libvirtd" "i2c" "podman" "input"];
+    extraGroups = [
+      "wheel" 
+      "networkmanager" 
+      "libvirtd"
+      "flatpak"
+      "audio"
+      "video"
+      "plugdev"
+      "input"
+      "kvm"
+      "qemu-libvirtd"
+      "i2c" 
+      "podman" 
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvfEm6EHx5a5J+WMxIpqaWYnqBDwXukKSwUmocfiYgg dyllant@Dyllans-Desktop"
     ];
